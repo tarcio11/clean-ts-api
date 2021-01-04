edd9839f0e299e0e82e95136bb3700425c322721
Tarcio <tarcio_x@hotmail.com>
2021-01-04 11:46:44 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -0,0 +1,9 @@
+import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultModel, SaveSurveyResult } from './db-save-survey-result-protocols'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel | any> {
+    await this.saveSurveyResultRepository.save(data)
+    return null
+  }
+}
--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -2,6 +2,6 @@ import { SurveyResultModel } from '../models/survey-result'
 
 export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
 
-export interface AddSurvey {
+export interface SaveSurveyResult {
   save (data: SaveSurveyResultModel): Promise<SurveyResultModel>
 }

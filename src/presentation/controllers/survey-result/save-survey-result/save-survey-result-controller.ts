ae3e38b5209a93030a6950556d7f639c9a3d762c
Tarcio <tarcio_x@hotmail.com>
2021-01-05 11:54:55 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,9 +1,13 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult } from './save-survey-result-controller-protocols'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyByIdStub: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyByIdStub: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
     try {
       const { surveyId } = httpRequest.params
@@ -17,6 +21,12 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.saveSurveyResult.save({
+        surveyId,
+        accountId: httpRequest.accountId as string,
+        answer,
+        date: new Date()
+      })
       return null
     } catch (error) {
       return serverError(error)

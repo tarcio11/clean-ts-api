1248cb423bc98fbf02a83420ba336d01fecc9b74
Tarcio <tarcio_x@hotmail.com>
2021-01-05 10:17:15 -0300

    feat: ensure SaveSurveyResultController returns 403 if invalid survey is provider

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,9 +1,14 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyByIdStub: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
-    await this.loadSurveyByIdStub.loadById(httpRequest.params.surveyId)
-    return null
+    const survey = await this.loadSurveyByIdStub.loadById(httpRequest.params.surveyId)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+    return survey
   }
 }

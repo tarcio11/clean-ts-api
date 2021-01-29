528b925520f7f05775569d9d94bfb1fff085f7c4
Tarcio Rocha <tarcio_x@hotmail.com>
2021-01-29 13:18:33 -0300

    refactor: remove all references to AccountModel

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,17 +1,24 @@
 import { MongoHelper } from '@/infra/db'
-import { AccountModel } from '@/domain/models'
 import { AddAccountRepository , LoadAccountByEmailRepository , LoadAccountByTokenRepository , UpdateAccessTokenRepository } from '@/data/protocols'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(data)
-    return MongoHelper.map(result.ops[0])
+    return result.ops[0] != null
   }
 
-  async loadByEmail (email: string): Promise<AccountModel | null> {
+  async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ email })
+    const account = await accountCollection.findOne({
+      email
+    }, {
+      projection: {
+        _id: 1,
+        name: 1,
+        password: 1
+      }
+    })
     return account && MongoHelper.map(account)
   }
 
@@ -19,8 +26,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.updateOne({
       _id: id
-    },
-    {
+    }, {
       $set: {
         accessToken: token
       }
@@ -36,7 +42,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
       }, {
         role: 'admin'
       }]
-    },{
+    }, {
       projection: {
         _id: 1
       }
